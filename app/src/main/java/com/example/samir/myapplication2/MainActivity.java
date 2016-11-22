3e781c07a1313c5bcd6220dcef291c0fe322e501
Samir Senhadri <sqf@o2.pl>
2016-11-22 23:18:13 +0100

    Multiplication with startActivityForResult

--- app/src/main/java/com/example/samir/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/samir/myapplication2/MainActivity.java
@@ -5,10 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText MultiplyEt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,18 +37,32 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Maps.class));
             }
         });
-
         final Button buttonMyActivity = (Button) findViewById(R.id.buttonMyActivity);
         buttonMyActivity.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Bundle extras = new Bundle();
-                extras.putString("kij", "janusz");
+                MultiplyEt = (EditText) findViewById(R.id.MultiplyEt);
+                String numberS = MultiplyEt.getText().toString();
+                extras.putString("kij", numberS);
                 Intent intent = new Intent(MainActivity.this, MyActivity.class);
                 intent.putExtras(extras);
                 startActivity(intent);
+                startActivityForResult(intent, 2);
             }
         });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        // check if the request code is same as what is passed  here it is 2
+        if(requestCode==2)
+        {
+            String message=data.getStringExtra("MESSAGE");
+            TextView textViewResult = (TextView) findViewById(R.id.textViewResult);
+            textViewResult.setText(message);
+        }
     }
 
 }
--- app/src/main/java/com/example/samir/myapplication2/MyActivity.java
+++ app/src/main/java/com/example/samir/myapplication2/MyActivity.java
@@ -1,11 +1,15 @@
 package com.example.samir.myapplication2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MyActivity extends AppCompatActivity {
-
+        EditText Multiply2Et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,6 +17,25 @@ public class MyActivity extends AppCompatActivity {
         String value = getIntent().getExtras().getString("kij");
         TextView textViewMyActivity = (TextView) findViewById(R.id.textViewMyActivity);
         textViewMyActivity.setText(value);
+
+        final Button buttonOk = (Button) findViewById(R.id.buttonOk);
+        buttonOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                Multiply2Et = (EditText) findViewById(R.id.MultiplyEt);
+                String number2S = Multiply2Et.getText().toString();
+                double number2D = Double.parseDouble(number2S);
+                String value = getIntent().getExtras().getString("kij");
+                double numberD = Double.parseDouble(value);
+                numberD = numberD * number2D;
+                Intent intent=new Intent();
+                String numberS = String.valueOf(numberD);
+                intent.putExtra("MESSAGE", numberS);
+                setResult(2,intent);
+                finish();//finishing activity
+            }
+        });
+
 
     }
 }
